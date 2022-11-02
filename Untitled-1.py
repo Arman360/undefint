a1444eaf0cc8286969deb4083a69475e3f43ea57
Ariful Islam <46670222+Arman360@users.noreply.github.com>
2022-11-02 15:21:32 +0600

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,7 @@
-import mailcap
-def 
+def join_us(name,rolNo):
+    return ("Hay " + name + " your form Goschi High school Your roll no is " + rolNo + " ?")
+
+num  = input("Name ")
+rolNo = input("Roll ")
+
+print(join_us(num, rolNo))
